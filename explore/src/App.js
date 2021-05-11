35dd881bd99bf59e8799ce446fb6254fe34f967f
Hamza <hamzazaman500@gmail.com>
2021-05-11 15:32:55 +0500

    basic-features

---
+++ explore/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Person from './components/Person';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello there</h1>
+    <Person/>
+    </div>
+    
+  );
+  
+}
+
+export default App;
---
+++ explore/src/components/Person.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+ const Person = () => {
+    return (
+        <div>
+            <p> I'm Hamza </p>
+        </div>
+    )
+}
+export default Person
+
+
+
+
