daf2c701e7e7f474a00ac4c742da498fdf54b6de
Hamza <hamzazaman500@gmail.com>
2021-05-12 13:44:53 +0500

    basic-features

--- explore/src/App.js
+++ explore/src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Person from './components/Person';
+class App extends Component {
+  state = {
+          people : [
+              {name:'Hamza', age:22},
+              {name:'Munsif', age:23},
+              {name:'Arslan', age:21}
+          ],
+          other:'other Data'
+        } 
 
-function App() {
-  return (
-    <div className="App">
-    <h1>Hello there</h1>
-    <Person/>
-    </div>
-    
-  );
-  
+          switchNameHandler = () =>{
+            this.setState({
+              people : [
+                  {name:'Hamza Zaman', age:22},
+                  {name:'Munsif', age:23},
+                  {name:'Arslan', age:19}
+              ]} )
+          }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello there</h1>
+     <button onClick={this.switchNameHandler}> Switch</button>
+     <Person name= {this.state.people[0].name} age={this.state.people[0].age}> My Hobby: Develope and learn new stuff daily. </Person>
+     <Person name= {this.state.people[1].name} age={this.state.people[1].age}/>
+     <Person name= {this.state.people[2].name} age={this.state.people[2].age}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- explore/src/components/Person.js
+++ explore/src/components/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
- const Person = () => {
+ const Person = (props) => {
     return (
         <div>
-            <p> I'm Hamza </p>
+            <p> I'm {props.name} and I'm {props.age} years old </p>
+            <p>{props.children}</p>
         </div>
     )
 }
